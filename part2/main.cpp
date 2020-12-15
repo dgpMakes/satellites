fa1519a434f6a53438181e7f4f02f1aae6fd2a69
raporpe <raporpe@gmail.com>
2020-12-15 12:57:30 +0100

    werk

--- part2/main.cpp
+++ part2/main.cpp
@@ -8,55 +8,58 @@
 std::vector<std::string> split(std::string s, std::string delimiter);
 void replace(std::string & data, std::string search, std::string replace);
 
-struct observation {
-    int time;
-    int band;
-};
+
 
 
 // // What every seach problem shall implement for the search algorithm to use
 class search_problem {
     public:
-        static search_problem* get_initial_state();
-        static search_problem* get_final_state();
-        std::vector<search_problem>* get_sucessors();
+        std::vector<search_problem>* get_successors();
         bool is_goal_state();
         ~search_problem();
 };
 
+// The only thing that has a cost is passing time
+enum action{observation, recharge, turn, downlink, nothing};
+
+struct node {
+    action sat1_action;
+    action sat2_action;
+    int accumulated_cost;
+    satellite_state state;
+    satellite_state* parent;
+};
+
+struct observation {
+    int time;
+    int band;
+};
+
 /*
 In this problem we are assuming there are only 12 hours on a day
-There are only 4 bands and three possitions for the two considered satellites.
+There are only 4 bands and 3 possitions for the two considered satellites.
 */
 
 class satellite_state {
     /* Dynamic variables */
     // Observations done
-    private:
-    std::vector<bool> is_obs_done;
-    int time;
-    int sat1_band;
-    int sat2_band;
-    bool downlinked;
-    int sat1_remaining_battery;
-    int sat2_remaining_battery;
+        
 
     public:
-
-    satellite_state(int time, int sat1_band, int sat2_band,
-     std::vector<bool> is_obs_done, bool downlinked) {
+        std::vector<bool> is_obs_done;
+        int time;
+        std::vector<int> sat_band;
+        std::vector<bool> downlinked;
+        std::vector<int> sat_remaining_battery;
+        
+    satellite_state(int time, std::vector<int> sat_band, std::vector<bool> downlinked,
+     std::vector<bool> is_obs_done, std::vector<int> sat_remaining_battery) 
+    {
         this->is_obs_done = is_obs_done;
-        
-        if(time >= 0 && time < 13){
-            this->time = time;
-        }
-        
-        this->sat1_band = sat1_band;
-        this->sat2_band = sat2_band;
+        this->time = time;
+        this->sat_band = sat_band;
         this->downlinked = downlinked;
-
-        this->sat1_remaining_battery = sat1_max_battery;
-        this->sat2_remaining_battery = sat2_max_battery;
+        this->sat_remaining_battery = sat_max_battery;
     }
 
     satellite_state() = default;
@@ -64,143 +67,179 @@ class satellite_state {
     
     /* Static parts */
     // Max battery capacity
-    static int sat1_max_battery;
-    static int sat2_max_battery;
+    static std::vector<int> sat_max_battery;
 
     // How much does battery increase per charge hour
-    static int sat1_battery_recharge;
-    static int sat2_battery_recharge;
+    static std::vector<int> sat_recharge_battery;
 
     // Cost of making an observation
-    static int sat1_observe_cost;
-    static int sat2_observe_cost;
+    static std::vector<int> sat_observe_cost;
 
     // Cost of downlinking
-    static int sat1_downlink_cost;
-    static int sat2_downlink_cost;
+    static std::vector<int> sat_downlink_cost;
 
     // Cost of visibility band turn
-    static int sat1_turn_cost;
-    static int sat2_turn_cost;
+    static std::vector<int> sat_turn_cost;
 
-    static std::vector<observation> obs_to_do;
+    static std::vector<struct observation> obs_to_do;
 
-    static satellite_state* get_initial_state(){
-        std::vector<bool> v (obs_to_do.size(), false);
-        return new satellite_state(0, 1, 3, v, false);
-    };
+        
+    
 
     // static satellite_state* get_final_state(){
     //     return satellite_state(0, 1, 3, new vector<bool> (obs_to_do.size(), true), true);
     // };
 
 
-    std::vector<satellite_state> get_sucessors() {
-        std::vector<satellite_state> v;
+    std::vector<node> get_successors() {
+        std::vector<node> v;
         /* Operations definition */
 
-        // Advance time
-        if(this->time < 12){
-            satellite_state s1(this->time + 1, this->sat1_band, this->sat2_band, this->is_obs_done, this->downlinked);
-            v.push_back(s1);
-        } else {
-            satellite_state s1(0, this->sat1_band, this->sat2_band, this->is_obs_done, this->downlinked);
-            v.push_back(s1);
-        }
+        // Definir un array de booleanos, indicando si un satélite puede hacer una acción.
+        std::vector<bool> sat1(5, false); // Observe, turn, recharge, downlink, do_nothing
+        std::vector<bool> sat2(5, false);
 
-        //satellite1
-        
-        bool observation(){
-        // Make an observation
-            //Check if there is enough battery
-            if(sat1_remaining_battery>sat1_observe_cost){
-           
-                //Check if there is something to observe
-                int i = 0;
-                int j = 0;
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(sat1[i] && sat2[j]) {
+                    // Add the operation
+                    int s_time = this->time + 1;
+                    std::vector<int> s_sat_band = sat_band;
+                    std::vector<bool> s_downlinked = downlinked;
+                    std::vector<int> s_sat_remaining_battery = sat_remaining_battery;
+                    std::vector<bool> s_is_obs_done = is_obs_done;
+                    action s1 = nothing;
+                    action s2 = nothing;
 
-                //Check if there is something to observe and if it has been observed
-                while(i<=3 && j < obs_to_do[i].size()){
-                    if(sat1_band == i && time == j && !is_obs_done[i][j]){
-                        is_obs_done[i][j] = true;
-                        sat1_remaining_battery -= sat1_observe_cost;
-                        i = 5;
+                    // Changing band
+                    if(i == 1){
+                        s_sat_band[0] = (sat_band[0] == 0 ? 1:0);
+                        s_sat_remaining_battery[0] -= satellite_state::sat_turn_cost[0];
+                        s1 = turn;
                     }
-                    i++;
+    
+                    if(j == 1){
+                        s_sat_band[1] = (sat_band[1] == 2 ? 1:2);
+                        s_sat_remaining_battery[1] -= satellite_state::sat_turn_cost[1];
+                        s2 = turn;
+                    }
+                    
+                    
+                    // Recharge action
+                    if(i == 2){
+                        s_sat_remaining_battery[0] += satellite_state::sat_recharge_battery[0];
+                        s1 = recharge;
+                    }
+                    if(j == 2) {
+                        s_sat_remaining_battery[1] += satellite_state::sat_recharge_battery[1];
+                        s2 = recharge;
+                    }
+                    
+                    // Downlink action
+                    if(i == 3){
+                        s_downlinked[0] = true;
+                        s_sat_remaining_battery[0] -= satellite_state::sat_downlink_cost[0];
+                        s1 = downlink;
+                    }
+                    if(j == 3){
+                        s_downlinked[1] = true;
+                        s_sat_remaining_battery[1] -= satellite_state::sat_downlink_cost[1];
+                        s2 = downlink;
+                    } 
+
+                    //If do nothing, nothing happens and time passes. 
+
+                    satellite_state s_state(s_time, s_sat_band, s_downlinked, s_is_obs_done, s_sat_remaining_battery);
+
+                    node n;
+                    n.sat1_action = s1;
+                    n.sat2_action = s2;
+                    n.parent = this;
+                    n.state = s_state;
+
+                    v.push_back(n);
+
                 }
             }
-            //Check if it has been already observed
         }
-        // Recharge
-            //Check if battery is at its maximum
 
-        // Turn to a different visibility band
-            //Check if there is enough battery
+  
+    
 
 
-        // Downlink information
-            //Check if there is enough battery
+        // for (int satellite=0; satellite<2; satellite++){
+            
+        //     int to_downlink[2];
+            
+        //     // Make an observation
+        //     //Check if there is enough battery
+        //     if(sat_remaining_battery[satellite]>sat_observe_cost[satellite]){
+        
+        //         //Check if there is something to observe
+        //         int i = 0;
+        //         int j = 0;
+        //         bool is_observed = false;
 
-
-
+        //         //Check if there is something to observe and if it has been observed
+        //         while(i<3 && j < obs_to_do.size()){
+        //             if(sat_band[satellite] == i && time == j && !is_obs_done[i][j]){
+        //                 // is_obs_done[i][satellitej] = true;
+        //                 sat_remaining_battery[satellite] -= sat_observe_cost[satellite];
+        //                 i = 5;
+        //                 //ok, se puede
+        //             }
+        //             i++;
+        //         }
+        //     }
+          
+        // }
 
         return v;
+        
     };
 
     bool is_goal_state() {
-        return downlinked;
+        return downlinked[0] && downlinked[1];
     };
 
     bool operator==(const satellite_state& other) {
         bool time_eq = this->time == other.time;
-        bool sat1_band_eq = this->sat1_band == other.sat1_band;
-        bool sat2_band_eq = this->sat2_band == other.sat2_band;
-        bool downlinked_eq = this->downlinked == other.downlinked;
+        bool sat1_band_eq = this->sat_band[0] == other.sat_band[0];
+        bool sat2_band_eq = this->sat_band[1] == other.sat_band[1];
+        bool sat1_downlinked_eq = this->downlinked[0] == other.downlinked[0];
+        bool sat2_downlinked_eq = this->downlinked[1] == other.downlinked[1];
+
         bool done_obs_eq = this->is_obs_done == other.is_obs_done;
-        return time_eq && sat1_band_eq && sat2_band_eq && downlinked_eq && done_obs_eq;
+        return time_eq && sat1_band_eq && sat2_band_eq && done_obs_eq;
     };
 
     friend std::ostream & operator<<(std::ostream & os, const satellite_state & foo){ 
-          os << (foo.downlinked ? "yes":"no") << foo.time << 
-          " sat1band:" << std::to_string(foo.sat1_band) << " sat2band:" << 
-          std::to_string(foo.sat2_band); 
+          os << (foo.downlinked[0] ? "yes":"no") << foo.time << 
+          " sat1band:" << std::to_string(foo.sat_band[0]) << " sat2band:" << 
+          std::to_string(foo.sat_band[1]); 
 
           return os;
     }
 };
 
 
-
-
-
-
-
-
-
     /* Static parts */
     // Cost of making an observation
-    int satellite_state::sat1_observe_cost = 0;
-    int satellite_state::sat2_observe_cost = 0;
+    std::vector<int> satellite_state::sat_observe_cost{2, 0};
 
     // Cost of downlinking
-    int satellite_state::sat1_downlink_cost = 0;
-    int satellite_state::sat2_downlink_cost = 0;
+    std::vector<int> satellite_state::sat_downlink_cost{0,0};
 
     // Cost of visibility band turn
-    int satellite_state::sat1_turn_cost = 0;
-    int satellite_state::sat2_turn_cost = 0;
+    std::vector<int> satellite_state::sat_turn_cost{0,0};
 
     // How much does battery increase per charge hour
-    int satellite_state::sat1_battery_recharge = 0;
-    int satellite_state::sat2_battery_recharge = 0;
+    std::vector<int> satellite_state::sat_recharge_battery{0,0};
 
     // Max battery capacity
-    int satellite_state::sat1_max_battery = 0;
-    int satellite_state::sat2_max_battery = 0;
-
-    /* Static parts */
+    std::vector<int> satellite_state::sat_max_battery{0,0};
     // Observations to do
-    std::vector<observation> satellite_state::obs_to_do;
+    std::vector<struct observation> satellite_state::obs_to_do;
 
 
 
@@ -219,30 +258,27 @@ class a_star {
 
     public:
         
-        a_star(satellite_state root){
-            this->root = root;
-        }
-
-        void search(){
-            std::vector<satellite_state> root_sucessors = root.get_sucessors();
-            for (satellite_state sp : root_sucessors){
-                queue.push(sp);
-            }
+        void search(satellite_state root){
+            node n;
+            n.accumulated_cost = 0;
+            n.parent = nullptr;
+            n.state = root;            
+            queue.push(n);
 
             // Infinite loop till a goal state is found
-            while(!queue.front().is_goal_state() && !queue.empty()){
+            while(!queue.front().state.is_goal_state() && !queue.empty()){
             
                 // Extract element from queue
-                satellite_state to_expand = queue.front();
+                node to_expand = queue.front();
                 queue.pop();
 
                 // Get all sucessors and append them to the fifo queue
-                std::vector<satellite_state> sucessors = to_expand.get_sucessors();
-                for(satellite_state sucessor : sucessors){
+                std::vector<node> sucessors = to_expand.state.get_successors();
+                for(node sucessor : sucessors){
                     // Check if the node has already been visited
-                    if(visited.find(sucessor) != visited.end()){
+                    if(visited.find(sucessor.state) != visited.end()){
                         queue.push(sucessor);
-                        visited.insert(sucessor);
+                        visited.insert(sucessor.state);
                     }
                 }
             }
@@ -252,14 +288,13 @@ class a_star {
                 return;
             }
 
-            std::cout << queue.front() << "\n Found a goal state!!!! \n";
-            std::cout << queue.front();
+            std::cout << queue.front().state << "\n Found a goal state!!!! \n";
+            std::cout << queue.front().state;
         }
 
     private:
-        std::queue<satellite_state> queue;
+        std::queue<node> queue;
         std::unordered_set<satellite_state> visited;
-        satellite_state root;
 
 
 
@@ -293,7 +328,7 @@ int main(int argc, char **argv) {
     std::vector<std::string> observations_list = split(lines[0], ";");
 
     // Transform each observation into a observation struct
-    std::vector<observation> observations;
+    std::vector<struct observation> observations;
     for(std::string obs : observations_list) {
 
         // Get rid of the separators
@@ -304,7 +339,7 @@ int main(int argc, char **argv) {
         std::vector<std::string> coordinates = split(obs, ",");
 
         // Create an observation and fill information
-        observation o;
+        struct observation o;
         o.time = std::stoi(coordinates[0]);
         o.band = std::stoi(coordinates[1]);
         observations.push_back(o);
@@ -326,31 +361,32 @@ int main(int argc, char **argv) {
 
     /* Static parts */
     // Cost of making an observation
-    satellite_state::sat1_observe_cost = std::stoi(sat1_data[0]);
-    satellite_state::sat2_observe_cost = std::stoi(sat2_data[0]);
+    satellite_state::sat_observe_cost[0] = std::stoi(sat1_data[0]);
+    satellite_state::sat_observe_cost[1] = std::stoi(sat2_data[0]);
 
     // Cost of downlinking
-    satellite_state::sat1_downlink_cost = std::stoi(sat1_data[1]);
-    satellite_state::sat2_downlink_cost = std::stoi(sat2_data[1]);
+    satellite_state::sat_downlink_cost[0] = std::stoi(sat1_data[1]);
+    satellite_state::sat_downlink_cost[1] = std::stoi(sat2_data[1]);
 
     // Cost of visibility band turn
-    satellite_state::sat1_turn_cost = std::stoi(sat1_data[2]);
-    satellite_state::sat2_turn_cost = std::stoi(sat2_data[2]);
+    satellite_state::sat_turn_cost[0] = std::stoi(sat1_data[2]);
+    satellite_state::sat_turn_cost[1] = std::stoi(sat2_data[2]);
 
     // How much does battery increase per charge hour
-    satellite_state::sat1_battery_recharge = std::stoi(sat1_data[3]);
-    satellite_state::sat2_battery_recharge = std::stoi(sat2_data[3]);
+    satellite_state::sat_recharge_battery[0] = std::stoi(sat1_data[3]);
+    satellite_state::sat_recharge_battery[1] = std::stoi(sat2_data[3]);
 
     // Max battery capacity
-    satellite_state::sat1_max_battery = std::stoi(sat1_data[4]);
-    satellite_state::sat2_max_battery = std::stoi(sat2_data[4]);
+    satellite_state::sat_max_battery[0] = std::stoi(sat1_data[4]);
+    satellite_state::sat_max_battery[2] = std::stoi(sat2_data[4]);
 
     /* Static parts */
     // Observations to do
     satellite_state::obs_to_do = observations;
-    satellite_state root = *satellite_state::get_initial_state();
-    a_star a(root);
-    a.search();
+    std::vector<bool> v (satellite_state::obs_to_do.size(), false);
+    // satellite_state root = satellite_state(0, 1, 3, v, false);
+    // a_star a(root);
+    // a.search();
 
 
     
